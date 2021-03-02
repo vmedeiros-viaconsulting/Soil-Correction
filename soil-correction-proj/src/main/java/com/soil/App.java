4cc26705d3f7aaa40265ed1f15c6d0946fc51c6c
Vinícius Medeiros da Silva <vinimed.98@gmail.com>
2021-03-02 17:01:45 -0300

    Add a calculation of Potassium

--- soil-correction-proj/src/main/java/com/soil/App.java
+++ soil-correction-proj/src/main/java/com/soil/App.java
@@ -4,10 +4,18 @@ package com.soil;
  * Hello world!
  *
  */
-public class App 
+public class App extends Potassio
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        Potassio k = new Potassio();
+
+        k.setTextureK(1);
+        if(k.getTextureK() == 1){
+            k.setValPotassio(0.35);
+        } else if(k.getTextureK() == 2){
+            k.setValPotassio(0.25);
+        }
+
+        System.out.println(k.getValPotassio());
     }
 }
---
+++ soil-correction-proj/src/main/java/com/soil/Potassio.java
@@ -0,0 +1,22 @@
+package com.soil;
+
+public class Potassio {
+  private double valPotassio;
+  private int textureK;
+
+  public int getTextureK(){
+    return textureK;
+  }
+
+  public void setTextureK(int textureK){
+    this.textureK = textureK;
+  }
+
+  public double getValPotassio(){
+    return valPotassio;
+  }
+
+  public void setValPotassio(double valPotassio){
+    this.valPotassio = valPotassio;
+  }
+}
